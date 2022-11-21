86381ac96eaf8f37f79debb66142d66a94c297a5
TalkingJello <TalkingJello@proton.me>
2022-11-21 17:14:22 +0200

    support ONA anime

--- src/dom/tmdbQueryFromPage.ts
+++ src/dom/tmdbQueryFromPage.ts
@@ -3,11 +3,12 @@ import { TmdbMediaType } from "../providers/TmdbProvider";
 export function tmdbQueryFromPage() {
   const s = $("#content > div.thin > h2:first-child").text().split(" - ");
   const post = s.pop();
-  const type = post.startsWith("TV Series")
-    ? TmdbMediaType.Tv
-    : post.startsWith("Movie")
-    ? TmdbMediaType.Movie
-    : false;
+  const type =
+    post.startsWith("TV Series") || post.startsWith("ONA")
+      ? TmdbMediaType.Tv
+      : post.startsWith("Movie")
+      ? TmdbMediaType.Movie
+      : false;
 
   if (!type) {
     return false;
