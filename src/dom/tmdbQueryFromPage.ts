0b423e9bb99fcb2ddc3d59cbb3c48224b56fb603
TalkingJello <TalkingJello@proton.me>
2022-11-28 19:37:03 +0200

    process title before tmdb query

--- src/dom/tmdbQueryFromPage.ts
+++ src/dom/tmdbQueryFromPage.ts
@@ -1,5 +1,18 @@
 import { TmdbMediaType } from "../providers/TmdbProvider";
 
+type Preprocessor = (title: string) => string;
+
+const preprocessors: Preprocessor[] = [
+  // Remove part number
+  (title) => title.replace(/Part \d+/i, ""),
+  // Remove season number
+  (title) => title.replace(/Season \d+/i, ""),
+  // Trim
+  (title) => title.trim(),
+  // Lowercase (so cache hits in case of different casing)
+  (title) => title.toLowerCase(),
+];
+
 export function tmdbQueryFromPage() {
   const s = $("#content > div.thin > h2:first-child").text().split(" - ");
   const post = s.pop();
@@ -17,6 +30,6 @@ export function tmdbQueryFromPage() {
   const name = s.join(" - ");
   return {
     type,
-    name,
+    name: preprocessors.reduce((acc, fn) => fn(acc), name),
   };
 }
