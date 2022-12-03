1d0b0800341f7488a944646f5b8104774ac4d7f0
TalkingJello <TalkingJello@proton.me>
2022-12-03 15:30:54 +0200

    improve user error notice

--- src/constants.ts
+++ src/constants.ts
@@ -13,4 +13,4 @@ export const ANIDB_CLIENT_VERSION = "1";
 export const TMDB_DEFAULT_API_KEY = "fe87c50cd11a52087a6e806623385b73";
 export const MAL_DEFAULT_API_KEY = "015cace9ce2a8dbd866dd7d9fa3ab561";
 export const internetOrWebsiteDownErrorTitle = (websiteName: string) =>
-  `Make sure you're connected to the internet and that ${websiteName} is not down.`;
+  `Make sure you're connected to the internet and that ${websiteName} is not down. Also make sure that the external ${websiteName} link is correct and leads to the anime's page.`;
